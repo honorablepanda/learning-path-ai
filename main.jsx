b78883f3921d2db0abd31a53db1f5048e9e0d0a5
Kashish Thesia <kthesia93@gmail.com>
2025-07-02 22:16:08 +0200

    fix: add ReactDOM root rendering entrypoint

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LearningPathCoach from "./index.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LearningPathCoach />
+  </React.StrictMode>
+);
